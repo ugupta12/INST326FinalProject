e5fea3c88011b5d73200f61d2d184f0236b52b1e
jramire4 <58197149+jramire4@users.noreply.github.com>
2019-12-11 20:58:25 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+from INST326FinalProject import basicinfo
+def test_player_Tom_Brady():
+    fullname = "Tom, Brady"
+    player = lastName, firstName
+    player_info = basicinfo(player)
+    Assert player_info == '39'
+    
+def test_player_Steve_Ramsey():
+    fullName = "Steve, Ramsey"
+    player = lastName, firstName
+    player_info = basicinfo(player)
+    Assert player_info == '70'
